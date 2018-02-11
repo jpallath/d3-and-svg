7ca75e247160a136298be85837511bad328053c8
Jerry Pallath <jpallath@nyu.edu>
2018-02-11 13:59:29 -0500

    more d3 practice

--- app.js
+++ app.js
@@ -36,12 +36,16 @@ d3.select("form")
         .exit()
         .remove();
 
-      letters
+      var letterEnter = letters
         .enter()
-        .append("rect")
+        .append("g")
           .classed("letter", true)
           .classed("new", true)
-        .merge(letters)
+      letterEnter.append("rect");
+      letterEnter.append("text");
+
+        letterEnter.merge(letters)
+          .select("rect")
           .style("width", barWidth)
           .style("height", function(d) {
             return d.count * 20;
@@ -53,6 +57,18 @@ d3.select("form")
             return height - d.count * 20;
           });
 
+        letterEnter.merge(letters)
+          .select("text")
+          .attr("x", function(d, i){
+            return(barWidth + barPadding) * i + barWidth / 2;
+          })
+          .attr("text-anchor","middle")
+          .attr("y", function(d){
+            return height - d.count * 20 - 10;
+          })
+          .text(function(d){
+            return d.character;
+          });
       d3.select("#phrase")
           .text("Analysis of: " + text);
 
